2dc88004e2209cccd98a19f2462473c3b58a5d42
Maja Frydrych <maja.frydrych@gmail.com>
2023-09-08 15:29:22 +0200

    add navbar component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,26 @@
 import { Route, Routes } from "react-router-dom";
-import { Home, About, Products, Blog, Contact } from "../components";
+import {
+	Navbar,
+	Footer,
+	Home,
+	About,
+	Products,
+	Blog,
+	Contact,
+} from "../components";
 
 export default function App() {
 	return (
-		<Routes>
-			<Route path='/' element={<Home />} />
-			<Route path='/about' element={<About />} />
-			<Route path='/products' element={<Products />} />
-			<Route path='/blog' element={<Blog />} />
-			<Route path='/contact' element={<Contact />} />
-		</Routes>
+		<>
+			<Navbar />
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/about' element={<About />} />
+				<Route path='/products' element={<Products />} />
+				<Route path='/blog' element={<Blog />} />
+				<Route path='/contact' element={<Contact />} />
+			</Routes>
+			<Footer />
+		</>
 	);
 }
