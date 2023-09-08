42515f4d40745256b1509d631601bc1253e6aaac
Maja Frydrych <maja.frydrych@gmail.com>
2023-09-08 15:38:16 +0200

    Add jsx to newsletter component

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,5 +1,20 @@
 import React from "react";
 
 export default function Newsletter() {
-	return <div>Newsletter</div>;
+	return (
+		<section className='subscribe section'>
+			<div className='container'>
+				<p className='section-title uppercase'>
+					Sign up and get free coffee bags
+				</p>
+				<h2 className='clr-neutral-100'>Coffee updates</h2>
+				<div className='flex flex-jc-c wrapper'>
+					<input type='text' />
+					<button className='clr-neutral-100 uppercase'>
+						Subscribe
+					</button>
+				</div>
+			</div>
+		</section>
+	);
 }
