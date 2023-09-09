1bd58fd860f700682456c6f627e77f6073654717
Maja Frydrych <maja.frydrych@gmail.com>
2023-09-09 19:45:31 +0200

    Add jsx to product details component

--- src/components/pages/ProductDetails.jsx
+++ src/components/pages/ProductDetails.jsx
@@ -1,5 +1,43 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { findProductById } from "../../utils/findProductById";
 
 export default function ProductDetails() {
-	return <div>ProductDetails</div>;
+	const { productId } = useParams();
+	const product = findProductById(productId);
+
+	return (
+		<>
+			<div>kkk</div>
+			<section className='details section'>
+				<div className='container'>
+					<div className='details__wrapper'>
+						<img
+							src={product.image}
+							alt=''
+							className='details__image'
+						/>
+						<div className='details__content'>
+							<h1 className='details__title'>{product.name}</h1>
+							<p className='details__description'>
+								{product.details}
+							</p>
+							<p className='details__price'>
+								$ {product.price}.00 USD
+							</p>
+							<div className='flex'>
+								<input
+									type='number'
+									className='details__input'
+								/>
+								<button className='secondary-btn uppercase'>
+									Add to card
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</>
+	);
 }
