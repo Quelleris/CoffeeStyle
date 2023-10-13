317ff5864a65ec8fbfad139a455514cf44d73941
Maja Frydrych <maja.frydrych@gmail.com>
2023-10-13 20:12:54 +0200

    Add empty cart message

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -4,18 +4,16 @@ import Product from "../Product/Product";
 
 export default function ProductList() {
 	return (
-		<section className='product section' id='product' aria-label='product'>
-			<div className='container'>
-				<ul className='grid-list product-list' data-filter='all'>
-					{PRODUCTS.map((product) => {
-						return (
-							<li key={product.id} className='product-list-item'>
-								<Product product={product} />
-							</li>
-						);
-					})}
-				</ul>
-			</div>
-		</section>
+		<div className='container'>
+			<ul className='grid-list product-list' data-filter='all'>
+				{PRODUCTS.map((product) => {
+					return (
+						<li key={product.id} className='product-list-item'>
+							<Product product={product} />
+						</li>
+					);
+				})}
+			</ul>
+		</div>
 	);
 }
