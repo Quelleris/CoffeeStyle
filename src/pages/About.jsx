3605705dfcfabba5df51bf2d07f963527e603e96
Maja Frydrych <maja.frydrych@gmail.com>
2023-10-16 10:30:50 +0200

    Add content to about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,141 @@ import React from "react";
 export default function About() {
 	return (
 		<>
-			<div>About</div>
+			<section className='about section'>
+				<div className='container'>
+					<div className='section-padding'>
+						<h2 className='h2 section-title'>About Us</h2>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing
+							elit. Suspendisse varius enim in eros elementum
+							tristique. Duis cursus, mi quis viverra ornare, eros
+							dolor interdum nulla, ut commodo diam libero vitae
+							erat.
+						</p>
+						<div className='img-wrapper'>
+							<img
+								src='https://assets.website-files.com/5be96251aaba7a7b19ecdf69/5be96251aaba7a780eecdfcc_About-Cover.jpg'
+								alt=''
+							/>
+						</div>
+
+						<p className='section-subtitle uppercase'>
+							Introductions
+						</p>
+						<h2 className='h2 section-title'>
+							Overlaid the jeepers uselessly much excluding
+							everyday life.
+						</h2>
+						<p>
+							It is a paradisematic country, in which roasted
+							parts of sentences fly into your mouth. Even the
+							all-powerful Pointing has no control about the blind
+							texts it is an almost unorthographic life One day
+							however a small line of blind text by the name of
+							Lorem Ipsum decided to leave for the far World of
+							Grammar.
+						</p>
+						<div className='grid-container'>
+							<div>
+								<h3 className='section-title'>
+									Overlaid the jeepers uselessly much
+									excluding everyday life.
+								</h3>
+								<p>
+									It is a paradisematic country, in which
+									roasted parts of sentences fly into your
+									mouth. Even the all-powerful Pointing has no
+									control about the blind texts it is an
+									almost unorthographic life One day however a
+									small line of blind text by the name of
+									Lorem Ipsum decided to leave for the far
+									World of Grammar.
+								</p>
+							</div>
+							<div>
+								<img
+									src='https://assets.website-files.com/5be96251aaba7a7b19ecdf69/5be96251aaba7af7e3ecdfad_About%20Image%201.jpg'
+									alt=''
+								/>
+							</div>
+						</div>
+						<div className='grid-container'>
+							<div>
+								<h3 className='section-title'>
+									Overlaid the jeepers uselessly much
+									excluding everyday life.
+								</h3>
+								<p>
+									It is a paradisematic country, in which
+									roasted parts of sentences fly into your
+									mouth. Even the all-powerful Pointing has no
+									control about the blind texts it is an
+									almost unorthographic life One day however a
+									small line of blind text by the name of
+									Lorem Ipsum decided to leave for the far
+									World of Grammar.
+								</p>
+							</div>
+							<div>
+								<img
+									src='https://assets.website-files.com/5be96251aaba7a7b19ecdf69/5be96251aaba7a6c4decdfa3_About%20Image%202.jpg'
+									alt=''
+								/>
+							</div>
+						</div>
+						<div className='section idk-img'></div>
+					</div>
+				</div>
+			</section>
+			<section className='about section'>
+				<div className='container'>
+					<p className='section-subtitle uppercase'>
+						History Timeline
+					</p>
+					<ul>
+						<li>
+							<date>October 2018</date>
+							<h3>One day however a small line</h3>
+							<p>
+								It is a paradisematic country, in which roasted
+								parts of sentences fly into your mouth. Even the
+								all-powerful Pointing has no control about the
+								blind texts it is an almost unorthographic life
+								One day however a small line of blind text by
+								the name of Lorem Ipsum.
+							</p>
+						</li>
+						<li>
+							<date>August 2018</date>
+							<h3>One day however a small line</h3>
+							<p>
+								It is a paradisematic country, in which roasted
+								parts of sentences fly into your mouth. Even the
+								all-powerful Pointing has no control about the
+								blind texts it is an almost unorthographic life
+								One day however a small line of blind text by
+								the name of Lorem Ipsum.
+							</p>
+						</li>
+						<li>
+							<date>June 2018</date>
+							<h3>One day however a small line</h3>
+							<p>
+								It is a paradisematic country, in which roasted
+								parts of sentences fly into your mouth. Even the
+								all-powerful Pointing has no control about the
+								blind texts it is an almost unorthographic life
+								One day however a small line of blind text by
+								the name of Lorem Ipsum.
+							</p>
+						</li>
+						<li>
+							<date>November 2017</date>
+							<h3>We've started CoffeeStyle.</h3>
+						</li>
+					</ul>
+				</div>
+			</section>
 		</>
 	);
 }
