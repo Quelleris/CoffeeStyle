be400ebd7c0d9ea84abbcfba4cf3feb2b614ed8e
Maja Frydrych <maja.frydrych@gmail.com>
2023-10-16 10:30:27 +0200

    Add main landmark

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,19 +17,21 @@ export default function App() {
 		<>
 			<CartContextProvider>
 				<Navbar />
-				<Routes>
-					<Route path='/' element={<Home />} />
-					<Route path='/about' element={<About />} />
-					<Route path='/products' element={<Products />} />
-					<Route
-						path='/products/:productId'
-						element={<ProductDetails />}
-					/>
-					<Route path='/blog' element={<Blog />} />
-					<Route path='/contact' element={<Contact />} />
-				</Routes>
-				<Newsletter />
-				<Footer />
+				<main>
+					<Routes>
+						<Route path='/' element={<Home />} />
+						<Route path='/about' element={<About />} />
+						<Route path='/products' element={<Products />} />
+						<Route
+							path='/products/:productId'
+							element={<ProductDetails />}
+						/>
+						<Route path='/blog' element={<Blog />} />
+						<Route path='/contact' element={<Contact />} />
+					</Routes>
+					<Newsletter />
+					<Footer />
+				</main>
 			</CartContextProvider>
 		</>
 	);
